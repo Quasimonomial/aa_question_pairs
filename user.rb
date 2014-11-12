cd6a7454b75651c3240df26a298b83bb1e5360ab
Travis Herrick <quasimonomial@gmail.com>
2014-11-11 22:29:36 -0800

    Added shit to questions database, create user model

---
+++ user.rb
@@ -0,0 +1,27 @@
+require_relative 'question'
+require_relative 'questions_database'
+require_relative 'question_follow'
+
+class User
+  def self.all
+    results = QuestionsDatabase.instance.execute('SELECT * FROM users')
+    resutls.map { |result| User.new(result) }
+  end
+  
+  def self.find_by_id id
+    attrs = { id: id }
+    result = QuestionsDatabase.instance.execute(
+      'SELECT * FROM users WHERE users.id = :id', attrs)
+  end
+
+  attr_accessor :id, :fname, :lname
+
+  def initialize(options = {})
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+
+
+
+end
